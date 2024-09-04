a3bb7c4a8ad28f65df14503c1f55503dde2454a7
webWave <lbwiththecodes@gmail.com>
2024-09-04 20:53:05 +0000

    Connect to DataBase

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,10 +1,21 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
+mongoose
+.connect(process.env.MONGO)
+.then(() => {
+    console.log('You are connected to MongoDB');
+})
+.catch((err) => {
+    console.log(err)
+});
 
 const app = express ();
 
 app.listen(3000,  () => {
-    console.log('running on port 3000')
+    console.log('running on port 3000!')
 }
 
 );
