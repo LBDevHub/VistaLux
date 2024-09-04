ce3a11119e0aa0d0c9b7e28823dacfc014dd5c45
webWave <lbwiththecodes@gmail.com>
2024-09-04 16:13:16 +0000

    Create first template and Install React js and tailwind Css

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <h1 className='text-3xl font-semibold text bg-slate-800'>
+       Oopps this is just the sketleton. 
+    </h1>
+  )
+}
+
