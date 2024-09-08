028150e273853aef40e29589c62b0c12b5d608c7
webWave <lbwiththecodes@gmail.com>
2024-09-08 11:51:13 +0000

    Updating Sign Up UI and Adding functionality

--- FrontEnd/src/Pages/SignUp.jsx
+++ FrontEnd/src/Pages/SignUp.jsx
@@ -1,9 +1,18 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const SignUp = () => {
   const [countryCode, setCountryCode] = useState('');
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    phonenumber: '',
+  });
 
   useEffect(() => {
     axios.get('https://ipapi.co/json/')
@@ -16,67 +25,106 @@ const SignUp = () => {
       });
   }, []);
 
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const fullFormData = { 
+        ...formData,
+        countryCode,
+      };
+    
+      const res = await fetch('/BackEnd/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(fullFormData),
+      });
+    
+      const data = await res.json();
+      console.log(data);
+      if(data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/sign-in');
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
   return (
     <div className="min-h-screen flex justify-center items-center bg-cover bg-center" style={{ backgroundImage: `url('./src/assets/tower.png')` }}>
       <div className="bg-white shadow-md rounded-lg p-6 w-full max-w-md">
         <h2 className="text-2xl font-bold text-center mb-6">Create your account</h2>
-        
+
+        {/* Social buttons */}
         <div className="mb-4">
           <button className="w-full bg-slate-200 text-black py-2 px-4 rounded-lg flex items-center justify-center">
             <img src="./src/assets/g.png" alt="Google" className="w-5 h-5 mr-2" />
             Sign Up with Google
           </button>
         </div>
-
         <div className="mb-4">
           <button className="w-full bg-blue-600 text-white py-2 px-4 rounded-lg flex items-center justify-center">
             <img src="./src/assets/In.png" alt="LinkedIn" className="w-5 h-5 mr-2" />
             Sign Up with LinkedIn
           </button>
         </div>
-        <div className="flex items-center justify-center mb-6">
-          <hr className="flex-grow border-gray-300" />
-          <span className="mx-2 text-gray-500">Or With Email and Password</span>
-          <hr className="flex-grow border-gray-300" />
-        </div>
 
-        <form>
+        {/* Form */}
+        <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label className="block text-gray-700">Username</label>
             <input
               type="text"
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#ffd700]"
-              placeholder="Username"
-            />
+              placeholder="Username" id='username' onChange={handleChange} />
           </div>
 
           <div className="mb-4">
-            <label className="block text-gray-700">Email Address</label>
             <input
               type="email"
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#ffd700]"
-              placeholder="Email"
-            />
+              placeholder="Email" id='email' onChange={handleChange} />
+          </div>
+
+          <div className="mb-4">
+            <input
+              type="password"
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#ffd700]"
+              placeholder="Create a password" id='password' onChange={handleChange} />
           </div>
 
           <div className="mb-6">
-            <label className="block text-gray-700">Phone Number</label>
             <div className="flex">
               <input
                 type="text"
                 value={countryCode}
                 className="w-1/4 px-4 py-2 border rounded-l-lg focus:outline-none focus:ring-2 focus:ring-[#ffd700]"
-                readOnly
-              />
+                readOnly id='countryCode' />
               <input
                 type="tel"
                 className="w-3/4 px-4 py-2 border rounded-r-lg focus:outline-none focus:ring-2 focus:ring-[#ffd700]"
-                placeholder="Enter your phone number"
-              />
+                placeholder="Enter your phone number" id='phonenumber' onChange={handleChange} />
             </div>
           </div>
 
-          <button className="w-full bg-[#ffd700] text-white py-2 px-4 rounded-lg">Sign Up</button>
+          <button disabled={loading}
+           className="w-full bg-[#ffd700] text-white py-2 px-4 rounded-lg uppercase hover:opacity-70 disabled:opacity-60">
+            {loading? 'Loading...' :  'Sign Up'}
+          </button>
         </form>
 
         <div className="mt-6 text-center flex gap-4">
@@ -85,6 +133,7 @@ const SignUp = () => {
             <span className="bg-slate-600 text-white py-1 px-2 rounded-lg cursor-pointer">Log In</span>
           </Link>
         </div>
+          {error && <p className='text-red-500 mt-5'>{error}'</p>}
       </div>
     </div>
   );
